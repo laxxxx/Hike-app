55292cf068a8440b95f33858adf30766d9f8c0ce
laxxxx <lakshmancool9@gmail.com>
2025-05-25 12:51:11 +0530

    initial commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Sree Lakshman on 25/05/25.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
