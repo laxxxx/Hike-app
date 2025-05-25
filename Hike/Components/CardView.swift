49123562d975283cf24771b9a6208666abf40829
laxxxx <lakshmancool9@gmail.com>
2025-05-25 13:49:50 +0530

    implement CardView

---
+++ Hike/Components/CardView.swift
@@ -0,0 +1,40 @@
+//
+//  CardView.swift
+//  Hike
+//
+//  Created by Sree Lakshman on 25/05/25.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        
+        // MARK: CardView
+        ZStack {
+            CustomBackgroundView()
+            ZStack {
+                Circle()
+                    .fill(
+                        LinearGradient(colors: [
+                            Color("ColorIndigoMedium"),
+                            Color("ColorSalmonLight")
+                        ],
+                                       startPoint: .topLeading,
+                                       endPoint: .bottomTrailing
+                                      )
+                    )
+                    .frame(width: 256, height: 256)
+                
+                Image("image-1")
+                    .resizable()
+                    .scaledToFit()
+            }
+        }
+        .frame(width: 320, height: 570)
+    }
+}
+
+#Preview {
+    CardView()
+}
---
+++ Hike/Utilities/ColorExtension.swift
@@ -0,0 +1,20 @@
+//
+//  ColorExtension.swift
+//  Hike
+//
+//  Created by Sree Lakshman on 25/05/25.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let customGreenLight = Color("ColorGreenLight")
+    static let customGreenMedium = Color("ColorGreenMedium")
+    static let customGreenDark = Color("ColorGreenDark")
+    static let customGrayDark = Color("ColorGrayDark")
+    static let customGrayLight = Color("ColorGrayLight")
+    static let customGrayMedium = Color("ColorGrayMedium")
+    static let customIndigoMedium = Color("ColorIndigoMedium")
+    static let customSalmonLight = Color("ColorSalmonLight")
+}
